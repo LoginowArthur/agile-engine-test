2c525facddfc3d2bb8d74098887fbcdef6f52e02
Arthur Loginow <loginowarthur@gmail.com>
2021-02-27 18:21:47 -0300

    basic http call, validation is ok, getting cors error

--- photos-gallery-app/src/app/app.component.ts
+++ photos-gallery-app/src/app/app.component.ts
@@ -1,8 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'pga-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  private agileEngineApiUrl = 'http://interview.agileengine.com';
+  private tokenUrl = 'http://interview.agileengine.com/auth';
+  private apiKey = '23567b218376f79d9415';
+
+  constructor(private http: HttpClient) {
+    this.http
+      .post(this.tokenUrl, { apiKey: this.apiKey })
+      .pipe(
+        switchMap((response: { auth: boolean; token: string }) => {
+          return this.http.get(this.agileEngineApiUrl, {
+            headers: { Authorization: `Bearer ${response.token}` },
+          });
+        })
+      )
+      .subscribe();
+  }
+}
