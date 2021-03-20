f4e8bdeee2a62796f3409341881cfce0e54d7dfd
Arthur Loginow <loginowarthur@gmail.com>
2021-03-20 20:53:13 -0300

    add global folder to be able to access global variables

---
+++ photos-gallery-app/src/app/global/gallery-api.ts
@@ -0,0 +1,3 @@
+export const AGINE_ENGINE_API_URL = 'http://interview.agileengine.com';
+export const TOKEN_URL = 'http://interview.agileengine.com/auth';
+export const API_KEY = '23567b218376f79d9415';
