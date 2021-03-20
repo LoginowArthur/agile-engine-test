537cd7e9bd1a8554806e78592b26162ac7607a49
Arthur Loginow <loginowarthur@gmail.com>
2021-03-20 20:54:47 -0300

    refactor component's logic, looks cleaner now

--- photos-gallery-app/src/app/components/gallery/gallery.component.ts
+++ photos-gallery-app/src/app/components/gallery/gallery.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http';
-import { Observable, of, Subject, zip } from 'rxjs';
-
-import { first, switchMap, tap, map } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+import { switchMap, map, first, tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ImagesApiService } from 'src/app/services/images-api/images-api.service';
 
 @Component({
   selector: 'pga-gallery',
@@ -12,68 +12,37 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
-  private agileEngineApiUrl = 'http://interview.agileengine.com';
-  private tokenUrl = 'http://interview.agileengine.com/auth';
-  private apiKey = '23567b218376f79d9415';
+  public pictures$ = new Observable<any[]>();
 
-  private bearerToken: string;
-  private bearerTokenSource = new Subject<string>();
-  private bearerToken$ = this.bearerTokenSource.asObservable();
-
-  public pictures = new Observable<any>();
-
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
-    this.getValidToken(this.tokenUrl, this.apiKey)
-      .pipe(
-        first(),
-        tap((response) => this.bearerTokenSource.next(response.token))
-      )
-      .subscribe();
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private imagesApiService: ImagesApiService
+  ) {
+    this.authService.setHeadersWithValidToken();
   }
 
   ngOnInit(): void {
-    zip(this.bearerToken$, this.route.paramMap)
-      .pipe(
-        first(),
-        map(([bearerToken, paramMap]) => {
-          this.bearerToken = bearerToken;
-          return { bearerToken: bearerToken, pageNumber: paramMap.get('page') };
-        }),
-        switchMap((response: { bearerToken: string; pageNumber: string }) => {
-          return this.getPicturesByPage(
-            response.bearerToken,
-            response.pageNumber
-          );
-        }),
-        tap((picturesResponse: { pictures: any[] }) => {
-          this.pictures = of(picturesResponse.pictures);
-        })
-      )
+    this.authService.waitForHeaders$
+      .pipe(tap(() => this.setPictures()))
       .subscribe();
   }
 
-  getValidToken(tokenUrl: string, apiKey: string): Observable<any> {
-    return this.http.post(tokenUrl, { apiKey: apiKey });
-  }
-
-  getPicturesByPage(bearerToken: string, galleryPage = '1') {
-    return this.http.get(
-      `${this.agileEngineApiUrl}/images?page=${galleryPage}`,
-      {
-        headers: { Authorization: `Bearer ${bearerToken}` },
-      }
-    );
-  }
-
-  getImageById(imageId: string, bearerToken) {
-    return this.http.get(`${this.agileEngineApiUrl}/images/${imageId}`, {
-      headers: {
-        Authorization: `Bearer ${bearerToken}`,
-      },
-    });
-  }
-
   onImageClicked(imageId: string) {
-    this.getImageById(imageId, this.bearerToken).pipe(first()).subscribe();
+    this.imagesApiService.getImageById(imageId).pipe(first()).subscribe();
+  }
+
+  setPictures() {
+    this.route.paramMap
+      .pipe(
+        map((paramMap) => paramMap.get('page')),
+        switchMap((pageNumber) =>
+          this.imagesApiService.getPicturesByPage(pageNumber)
+        ),
+        tap((response: { pictures: any[] }) => {
+          this.pictures$ = of(response.pictures);
+        })
+      )
+      .subscribe();
   }
 }
