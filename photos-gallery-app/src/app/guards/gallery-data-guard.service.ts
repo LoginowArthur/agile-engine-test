1d0d090c0e5ab5615280cb93611d379da8a1f739
Arthur Loginow <loginowarthur@gmail.com>
2021-03-21 23:55:19 -0300

    add guards to validate headers in the router

---
+++ photos-gallery-app/src/app/guards/gallery-data-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class GalleryDataGuardService implements CanActivate {
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    console.log('can activate');
+    return this.authService.waitForHeaders$;
+  }
+}
