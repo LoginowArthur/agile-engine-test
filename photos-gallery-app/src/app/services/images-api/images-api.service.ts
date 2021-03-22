b867f039868398f58fffd1710c83e0d8d198bc8b
Arthur Loginow <loginowarthur@gmail.com>
2021-03-21 23:59:30 -0300

    modify methods names default values

--- photos-gallery-app/src/app/services/images-api/images-api.service.ts
+++ photos-gallery-app/src/app/services/images-api/images-api.service.ts
@@ -12,14 +12,14 @@ export class ImagesApiService {
   constructor(private http: HttpClient, private authService: AuthService) {}
 
   // Remove any as generic type
-  getPicturesByPage(galleryPage = 'headers1'): Observable<any> {
+  getPicturesByPage$(galleryPage = '1'): Observable<any> {
     return this.http.get(`${AGINE_ENGINE_API_URL}/images?page=${galleryPage}`, {
       headers: this.authService.headers,
     });
   }
 
   // Remove any as generic type
-  getImageById(imageId: string): Observable<any> {
+  getImageById$(imageId: string): Observable<any> {
     return this.http.get(`${AGINE_ENGINE_API_URL}/images/${imageId}`, {
       headers: this.authService.headers,
     });
