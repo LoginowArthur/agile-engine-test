d934736670d7d4ca1734ea2af9303f56d1b30ae4
Arthur Loginow <loginowarthur@gmail.com>
2021-03-21 23:57:06 -0300

    add guard and resolver to gallery route

--- photos-gallery-app/src/app/app-routing.module.ts
+++ photos-gallery-app/src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GalleryItemComponent } from './components/gallery-item/gallery-item.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { GalleryDataGuardService } from './guards/gallery-data-guard.service';
+import { GalleryData } from './resolvers/gallery-data.resolver';
 
 const routes: Routes = [
-  { path: 'gallery/:page', component: GalleryComponent },
-  { path: 'photo/:id', component: GalleryItemComponent },
+  {
+    path: 'gallery/:pageNumber',
+    component: GalleryComponent,
+    canActivate: [GalleryDataGuardService],
+    resolve: { galleryData: GalleryData },
+  },
   { path: '', redirectTo: 'gallery/1', pathMatch: 'full' },
   { path: '**', redirectTo: 'gallery/1' },
+  {
+    path: 'picture/:id',
+    outlet: 'gallery-item',
+    component: GalleryItemComponent,
+  },
 ];
 
 @NgModule({
