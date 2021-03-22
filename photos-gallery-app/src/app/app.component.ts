489351254f7fe1d008699032d278ccf281d4f40f
Arthur Loginow <loginowarthur@gmail.com>
2021-03-21 23:58:36 -0300

    set token inside app component and add aux route outlet

--- photos-gallery-app/src/app/app.component.ts
+++ photos-gallery-app/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'pga-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private authService: AuthService) {
+    this.authService.setHeadersWithValidToken();
+  }
+}
